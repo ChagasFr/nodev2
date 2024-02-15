3755a9f1c2e9368b0167985611dd0146dc1796b4
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-02-15 12:41:08 -0300

    feat: compressao de imgs

--- app/app.js
+++ app/app.js
@@ -1,4 +1,31 @@
+const sharp = require('sharp')
+
 let path = process.argv[2];
 let width = Number(process.argv[3]);
 
-console.log(path, width)
+function resize(path, width) {
+    sharp(path).resize({width: width}).toFile('./temp/output_resize.jpg', (err) => {
+        if(err) {
+            console.log(err)
+        } else {
+            console.log('boa')
+            compress(outputPath, "./compressed")
+        }
+    })
+}
+
+function compress(pathInput, outputPath) {
+    compress_images(pathInput, outputPath, {compress_force: false, statictic: false, autoupdade: true}, false,
+        { jpg: { engine: 'mozjpeg', command: ['-quality', '60'] } },
+        { png: { engine: 'pngquant', command: ['--quality=20-5-'] } },
+        { svg: { engine: 'svgo', command: '--multipass' } },
+        { gif: { engine: 'gifsicle', command: ['--colors', '64', '--use-col=web'] } }, function (error, completed, statistics) {
+            console.log('----------');
+            console.log(error);
+            console.log(completed);
+            console.log(statistics);
+            console.log('---------');
+        });
+}
+
+resize(path, './temp/output_resize.jpg', width)
