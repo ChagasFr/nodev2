b48b03c6c11e113c6d31506f7c801a24d33e6ffe
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-02-16 19:16:01 -0300

    feat: servidor de paginas

--- serverweb/index.js
+++ serverweb/index.js
@@ -5,12 +5,21 @@ const fs = require('fs')
 // criando a solicitacao e recebendo a resposta
 http.createServer((request, response) => {
     let path = url.parse(resquest.url).pathname;
+    
+    if(path == "" || path == "/") {
+        path = "/index.html";
+    }
+    
     let fileName = "." +  path;
-
+    
     fs.readFile(fileName, (err, data ) => {
         if(err) {
             response.writeHead(404, {"content-type":"text/html"});
             response.end("pagina nao encontrada")
+        } else {
+            response.writeHead(200, {"content-type":"text/html"});
+            response.write(data);
+            response.end(end);
         }
     })
     response.end()
