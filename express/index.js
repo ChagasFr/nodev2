56615a7742f896eead6d4fa0f465d75000b0d516
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-02-18 00:21:07 -0300

    feat: desenvoldendo o get pelo express

--- express/index.js
+++ express/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.use("/meusite", express.static(path.join(__dirname, 'client')));
+
+app.get("/", (req, res) => {
+    res.send("hello word")
+})
+
 const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`server running on port:${PORT}`)
