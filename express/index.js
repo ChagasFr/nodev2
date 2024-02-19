b49374e98bfcdc39bd0358b86bc8678c89b0c2ea
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-02-18 21:43:39 -0300

    feat: desenvolvendo os 4 verbos com express

--- express/index.js
+++ express/index.js
@@ -5,7 +5,19 @@ const app = express();
 app.use("/meusite", express.static(path.join(__dirname, 'client')));
 
 app.get("/", (req, res) => {
-    res.send("hello word")
+    res.send("<h1>Hello word from get<h1>");
+})
+
+app.post("/", (req, res) => {
+    res.send("<h1>Hello word from post<h1>");
+})
+
+app.put("/", (req, res) => {
+    res.send("<h1>Hello word from put<h1>");
+})
+
+app.delete("/", (req, res) => {
+    res.send("<h1>Hello word from delete<h1>");
 })
 
 const PORT = 5000;
